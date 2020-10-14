06a413e8171fe3cfae60928ead7293d54027ac3e
James Rodgers <myfavoritenoisemaker@gmail.com>
2020-10-13 20:04:18 -0600

    added other problem builder functions

--- src/getProblems.js
+++ src/getProblems.js
@@ -10,3 +10,70 @@ export function createTimesTable() {
     }
     return problems
 }
+
+export function createAdditionTable() {
+    let problems = []
+    for (let i = 1; i < 21; i++) {
+        for (let a = 1; a < 21; a++) {
+            problems.push({
+                "problem": `${a} + ${i}`,
+                "answer": a + i
+            })
+        }
+    
+    return problems
+}
+
+export function createDivisionTable() {
+    let problems = []
+    for (let i = 1; i < 11; i++) {
+        for (let a = 1; a < 11; a++) {
+            let dividend = a * i
+            problems.push({
+                "problem": `${dividend} / ${a}`,
+                "answer": i
+            })
+        }
+    }
+    return problems
+}
+
+export function createSubtractionTable() {
+    let problems = []
+    for (let i = 1; i < 21; i++) {
+        for (let a = 1; a < 21; a++) {
+            let sum = a + i
+            problems.push({
+                "problem": `${sum} - ${a}`,
+                "answer": i
+            })
+        }
+    }
+    return problems
+}
+
+export function createAdditionTable100() {
+    let problems = []
+    for (let i = 1; i < 101; i++) {
+        for (let a = 1; a < 101; a++) {
+            problems.push({
+                "problem": `${a} + ${i}`,
+                "answer": a + i
+            })
+        }
+    return problems
+}
+
+export function createSubtractionTable100() {
+    let problems = []
+    for (let i = 1; i < 101; i++) {
+        for (let a = 1; a < 101; a++) {
+            let sum = a + i
+            problems.push({
+                "problem": `${sum} - ${a}`,
+                "answer": i
+            })
+        }
+    }
+    return problems
+}
