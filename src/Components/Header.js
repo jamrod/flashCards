884ce163cf67c1d82c38c82a963da781a0b98117
Jamrod <myfavoritenoisemaker@gmail.com>
2020-10-14 15:08:08 -0600

    styled, with background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,18 @@ import '../App.css'
 
 function Header() {
     return (
-        <div className="flex-container-row centered">
-            <h1>Flash Cards</h1>
+        <div className="flex-container-row centered header">
+            <div className="letters red"><h2>F</h2></div>
+            <div className="letters tilt-r"><h2>L</h2></div>
+            <div className="letters tilt"><h2>A</h2></div>
+            <div className="letters red"><h2>S</h2></div>
+            <div className="letters tilt-r"><h2>H</h2></div>
+            <div className="letters red"><h2>C</h2></div>
+            <div className="letters tilt"><h2>A</h2></div>
+            <div className="letters red"><h2>R</h2></div>
+            <div className="letters tilt-r"><h2>D</h2></div>
+            <div className="letters red"><h2>S</h2></div>
+            {/* <h1>Flash Cards</h1> */}
         </div>
     )
 }
